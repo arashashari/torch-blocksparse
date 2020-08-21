feaf297ad75c79d36b8501ec32cf7c6290040e13
arashashari <arash.ashari@gmail.com>
2020-08-21 21:16:20 +0000

    updated module list based on new util functions

--- torch_blocksparse/__init__.py
+++ torch_blocksparse/__init__.py
@@ -8,4 +8,4 @@ from .bertsparseselfattention import BertSparseSelfAttention
 from .batchnorm import BatchNorm2d
 from .permute import _permute, Permute
 from .relu import ReLU
-from .utils import extend_position_embedding, update_tokenizer_model_max_length, replace_model_self_attention_with_sparse_self_attention, replace_self_attention_layer_with_sparse_self_attention_layer
+from .utils import extend_position_embedding, update_tokenizer_model_max_length, replace_model_self_attention_with_sparse_self_attention, replace_self_attention_layer_with_sparse_self_attention_layer, pad_to_block_size, unpad_sequence_output
